049df8170c0f0c36f2a2c8bd2d5e31ea4295ae93
manju <manju@LAPTOP-0BPN2JK4>
2025-01-09 23:23:24 +0530

    push

--- TestMainClass.java
+++ TestMainClass.java
@@ -3,8 +3,7 @@ public class TestMainClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World Commit1s");
-		System.out.println("Hello World Commit2s");
-		System.out.println("Hello World Commit3s");
+		
 		}
 
 	}
